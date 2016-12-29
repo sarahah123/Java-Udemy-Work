21db249f781d40699154a4c067a714a0d7c982ea
sarahah123 <shansber55@gmail.com>
2016-12-29 17:48:19 -0500

    Add tutorial 2

--- Tutorial1/src/Application.java
+++ Tutorial1/src/Application.java
@@ -4,7 +4,25 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello World\n");
 		
-		int myNumber;
-		int number2;
+		int myNumber = 88;
+		short myShort = 847;
+		long myLong = 9765;
+		
+		double myDouble = 7.321456;
+		float myFloat = 324.56f;
+		
+		char myChar = 'd';
+		boolean myBoolean = true;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
 	}
 }
