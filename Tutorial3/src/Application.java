f450363d29548133bc3a4a9f074c58a8f6f3507d
sarahah123 <shansber55@gmail.com>
2016-12-29 18:02:02 -0500

    Add tutorial 3

---
+++ Tutorial3/src/Application.java
@@ -0,0 +1,24 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		double myDouble = 7.8;
+		
+		String text = "Hello";
+		
+		String blank = " ";
+		
+		String name = "Tina";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(text);
+		
+		System.out.println("Hello" + " " + "Bob");
+		
+		System.out.println("My integer is: " + myInt);
+				
+		System.out.println("My number is " + myDouble + ".");
+	}
+}
