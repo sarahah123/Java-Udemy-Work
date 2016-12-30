927a373fccfc5a4a8d9006f8d8aff8dbae558733
sarahah123 <shansber55@gmail.com>
2016-12-30 01:59:36 -0500

    Add tutorial 6

---
+++ Tutorial6/src/Application.java
@@ -0,0 +1,33 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		int myInt = 15;
+		
+		if(myInt < 10) {
+			System.out.println("Yes, it's true.");
+		}
+		else if(myInt > 20){
+			System.out.println("No, it's false.");
+		}
+		else {
+			System.out.println("None of the above");
+		}
+		
+		System.out.println("\nPart 2\n");
+		
+		int loop = 0;
+		
+		while(true) {
+			System.out.println("Looping: " + loop);
+			
+			if(loop == 5) {
+				break;
+			}
+			
+			loop++;
+			
+			System.out.println("Running");
+		}
+	}
+}
