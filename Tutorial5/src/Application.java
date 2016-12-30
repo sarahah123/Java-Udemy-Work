d160f3cc5cddb9f4deabee59e2b7b5e9438711c4
sarahah123 <shansber55@gmail.com>
2016-12-30 01:45:26 -0500

    Revert "Revert "Add tutorial 5""
    This reverts commit 91f14c25b30384d2d8c8e8f282556bb188d0d193.

--- Tutorial5/src/Application.java
+++ Tutorial5/src/Application.java
@@ -2,7 +2,7 @@ public class Application {
 	public static void main(String[] args) {
 		
 		for(int i=0; i < 5; i++){
-			System.out.printf("The value of i is: %d", i);
+			System.out.printf("The value of i is: %d\n", i);
 		}
 	}
 }
