22f5fa3a7aa33a916257628e7f96d8c132648a01
sarahah123 <shansber55@gmail.com>
2017-01-03 03:10:17 -0500

    Add Tutorial 11

---
+++ Tutorial 11/src/Application.java
@@ -0,0 +1,29 @@
+
+public class Application {
+	public static void main(String[] args) {
+		String[] words = new String[3];
+		words[0] = "hello";
+		words[1] = "to";
+		words[2] = "you";
+		
+		System.out.println(words[2]);
+		
+		String[] fruits = {"Apple", "banana", "pear", "kiwi"};
+		
+		for(String fruit: fruits){
+			System.out.println(fruit);
+		}
+		
+		int values = 0;
+		
+		String text = null;
+		
+		System.out.println(text);
+		
+		String[] texts = new String[2];
+		
+		System.out.println(texts[0]);
+		
+		texts[0] = "one";
+	}
+}
