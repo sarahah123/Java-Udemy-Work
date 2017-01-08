412bae5ad7ec11dba25c48e0a498b606977ce73d
sarahah123 <shansber55@gmail.com>
2017-01-07 22:19:21 -0500

    Update Tutorial 13

--- Tutorial 13/src/Application.java
+++ Tutorial 13/src/Application.java
@@ -10,8 +10,17 @@ class Person{
 	1. Data
 	2. Subroutines (methods)
 	*/
+	
+	void speak(){
+		for(int i=0; i<3; i++){
+			System.out.println("Hello, my name is " + name + "and I am " + age + ".");
+		}
+	}
+	
+	void sayHello(){
+		System.out.println("Hello there!");
+	}
 }
-
 
 
 public class Application {
@@ -20,11 +29,16 @@ public class Application {
 		
 		person1.name = "Joe Bloggs";
 		person1.age = 37;
+		person1.speak();
+		person1.sayHello();
+		
 		
 		Person person2 = new Person();
 		
 		person2.name = "Gavin Burns";
 		person2.age = 27;
+		person2.speak();
+		person2.sayHello();
 
 	}
 }
